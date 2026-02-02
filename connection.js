fde7434671d4c814cc7f0d475d66da47916b19ae
Adarsh <adarsh1o1@Adarshs-iMac.lan>
2026-02-03 00:52:53 +0530

    bugs fixes

--- connection.js
+++ connection.js
@@ -1,7 +1,15 @@
-const { connect } = require("mongoose");
+const mongoose = require("mongoose");
 
-async function connectToDb(url) {
-  return connect(url);
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+
+async function connectToDb(uri) {
+  try {
+    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+    await mongoose.connect(uri, clientOptions);
+  }
+  catch (error){
+    console.log(error);
+  }
 }
 
 module.exports = { connectToDb };
--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const userRouter = require("./routes/user");
-const {connectToDb} = require("./connection");
-const {logReqRes} = require("./middlewares/log");
+const { connectToDb } = require("./connection");
+const { logReqRes } = require("./middlewares/log");
 const cors = require("cors");
 const path = require("path");
-
-
 
 const app = express();
 const PORT = 8000;
 
-connectToDb("mongodb://localhost:27017/dropp").then(()=> console.log("MongoDb connected!"));
+connectToDb("mongodb+srv://adarshkushawha52_db_user:tQRxfzBS2SroX3DW@dropp.80z14dt.mongodb.net/?appName=Dropp").then(()=> console.log("MongoDb connected!"));
+
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.resolve("./public")));
 app.use(logReqRes("log.txt"));
 
 app.use("/user", userRouter);
 
-
-app.listen(PORT, () =>{ console.log(`Server started at port ${PORT}`)});
+app.listen(PORT, () => {
+  console.log(`Server started at port ${PORT}`);
+});
